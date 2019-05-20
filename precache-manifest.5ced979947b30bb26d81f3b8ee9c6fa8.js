13c9664c7e9977873baac11e06a844d25a95494e
unknown <ragavantar@gmail.com>
2019-05-20 12:25:08 +0530

    Updates

---
+++ precache-manifest.5ced979947b30bb26d81f3b8ee9c6fa8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3158250067df2defe12abc7d846c9fbc",
+    "url": "/travel_store_template/index.html"
+  },
+  {
+    "revision": "da2fde31147d9405d809",
+    "url": "/travel_store_template/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c2a20fa9004fc7c82ce3",
+    "url": "/travel_store_template/static/css/main.715d8d40.chunk.css"
+  },
+  {
+    "revision": "da2fde31147d9405d809",
+    "url": "/travel_store_template/static/js/2.49a92fc4.chunk.js"
+  },
+  {
+    "revision": "c2a20fa9004fc7c82ce3",
+    "url": "/travel_store_template/static/js/main.4e418613.chunk.js"
+  },
+  {
+    "revision": "7ba8a540c07f484efbcb",
+    "url": "/travel_store_template/static/js/runtime~main.3366be80.js"
+  }
+]);
